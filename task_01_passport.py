56169e69d3f95ebd15f1ccdaace1ca0e328854dc
Anna <amkhvnn@yandex.ru>
2026-09-19 22:31:50 +0300

    Сделано первое задание

---
+++ task_01_passport.py
@@ -0,0 +1,19 @@
+student_name = "Амирханова Анна" #ФИО
+group_number = 52501 #Номер группы
+project_name = "ЖК Ромашки" #Название объекта
+floors = 18 #Этажность
+height = 54 #Высота
+is_residential = True #Тип
+construction_year = 2023 #год постройки
+
+print(f"== ПАСПОРТ СТРОИТЕЛЬНОГО ОБЪЕКТА ==\n"
+      f"Составитель: {student_name}\n"
+      f"Группа: {group_number}\n"
+      f"Объект: {project_name}\n"
+      f"Этажность: {floors} этажей\n"
+      f"Высота: {height} м\n"
+      f"Тип: {'Жилой' if is_residential else 'Нежилой'}\n"
+      f"Год постройки: {construction_year}")
+
+# Шоссе в Лаврики, д. 93
+# Я живу рядом
